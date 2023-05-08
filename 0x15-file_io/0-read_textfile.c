6b3cb83675b93aefbfa439b5ec9f865eb2a30f1d
Ikrame9 <ikchkc@gmail.com>
2023-05-08 07:00:07 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 
 /**
- * read_textfile - reads a text file and prints it to stdout
- * @filename: the name of the file to read
- * @letters: the number of characters to read from the file
- *
- * Return: the number of characters read and printed to stdout,
- *         or 0 if an error occurs
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
 	char *buf;
-	ssize_t nread, nwritten;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
 	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		fclose(fp);
-		return (0);
-	}
-
-	nread = fread(buf, sizeof(char), letters, fp);
-	nwritten = fwrite(buf, sizeof(char), nread, stdout);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
-	fclose(fp);
-
-	return (nwritten);
+	close(fd);
+	return (w);
 }
+
